cc716957cef578334f09642b2226d36bd787fd45
Maksim Dragun <maksimdragun@live.com>
2019-11-07 13:07:07 +0300

    game life: c/10 ship

--- ledecorator-core/src/apps/games/life/lifegame.cpp
+++ ledecorator-core/src/apps/games/life/lifegame.cpp
@@ -7,9 +7,7 @@ LifeGame::LifeGame(uint8_t color_life, uint8_t color_dead)
 	this->color_life = color_life;
 	this->color_dead = color_dead;
 	this->alive_indicator = ALIVE_INDICATOR_01;
-	place_entity(0, 0, alive_indicator);
-	place_entity(12, 7, alive_indicator);
-	place_entity(25, 13, alive_indicator);
+	place_ship(0, 4, alive_indicator);
 }
 
 bool LifeGame::is_running()
@@ -25,8 +23,9 @@ void LifeGame::increment()
 
 	for (uint8_t y = 0; y < SCREEN_HEIGHT; y++)
 	{
-		uint8_t top = y == 0 ? SCREEN_HEIGHT - 1 : y - 1;
-		uint8_t bottom = y == SCREEN_HEIGHT - 1 ? 0 : y + 1;
+		uint8_t* top_row = field[y == 0 ? SCREEN_HEIGHT - 1 : y - 1];
+		uint8_t* current_row = field[y];
+		uint8_t* bottom_row = field[y == SCREEN_HEIGHT - 1 ? 0 : y + 1];
 		for (uint8_t x = 0; x < SCREEN_WIDTH; x++)
 		{
 			uint8_t alive_neigbhoors = 0;
@@ -34,17 +33,40 @@ void LifeGame::increment()
 			uint8_t right = x == SCREEN_WIDTH - 1 ? 0 : x + 1;
 
 
-			alive_neigbhoors += (field[top][left] & alive_indicator ? 1 : 0); 		// TOP LEFT
-			alive_neigbhoors += (field[top][x] & alive_indicator ? 1 : 0); 			// TOP
-			alive_neigbhoors += (field[top][right] & alive_indicator ? 1 : 0); 		// TOP RIGHT
-			alive_neigbhoors += (field[y][right] & alive_indicator ? 1 : 0); 		// RIGHT
-			alive_neigbhoors += (field[bottom][right] & alive_indicator ? 1 : 0); 	// BOTTOM RIGHT
-			alive_neigbhoors += (field[bottom][x] & alive_indicator ? 1 : 0);		// BOTTOM
-			alive_neigbhoors += (field[bottom][left] & alive_indicator ? 1 : 0); 	// BOTTOM LEFT
-			alive_neigbhoors += (field[y][left] & alive_indicator ? 1 : 0); 		// LEFT
+			if (top_row[left] & alive_indicator)
+			{
+				alive_neigbhoors++;
+			}
+			if (top_row[x] & alive_indicator)
+			{
+				alive_neigbhoors++;
+			}
+			if (top_row[right] & alive_indicator)
+			{
+				alive_neigbhoors++;
+			}
+			if (current_row[right] & alive_indicator)
+			{
+				alive_neigbhoors++;
+			}
+			if (bottom_row[right] & alive_indicator)
+			{
+				alive_neigbhoors++;
+			}
+			if (bottom_row[x] & alive_indicator)
+			{
+				alive_neigbhoors++;
+			}
+			if (bottom_row[left] & alive_indicator)
+			{
+				alive_neigbhoors++;
+			}
+			if (current_row[left] & alive_indicator)
+			{
+				alive_neigbhoors++;
+			}
 
 			uint8_t cell = field[y][x];
-
  			field[y][x] = (alive_neigbhoors == 3 || (alive_neigbhoors == 2 && (cell & alive_indicator))) ?
 					cell | next_alive_indicator : cell & next_dead_indicator;
 		}
